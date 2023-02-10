72732f8e2eb8c4a908ffd766bd965f26ebecc0eb
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-10 00:02:29 -0300

    change cursor for vi mode

--- fish/config.fish
+++ fish/config.fish
@@ -3,4 +3,9 @@ if status is-interactive
 
   function fish_greeting
   end
+
+  set fish_cursor_default block
+  set fish_cursor_insert line
+  set fish_cursor_replace underscore
+  set fish_cursor_replace_one underscore
 end
