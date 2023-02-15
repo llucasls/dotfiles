1df147a74cc844a3dce82b3b60ceadc52e0b812c
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-15 01:01:25 -0300

    export environment variable LESS

--- fish/config.fish
+++ fish/config.fish
@@ -8,4 +8,6 @@ if status is-interactive
   set fish_cursor_insert line
   set fish_cursor_replace underscore
   set fish_cursor_replace_one underscore
+
+  set -xg LESS '-R~ --use-color -Dd+r$Du+B'
 end
