d121ebc388604733d238c97bc4c319c7c0ef30e2
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-16 00:54:44 -0300

    edit fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,26 @@
 if status is-interactive
-  starship init fish | source
-
   function fish_greeting
   end
+
+  function starship_transient_prompt_func
+    set -f prompt
+    set -a prompt $(starship module shell | string trim)
+    set -a prompt $(starship module directory | string trim)
+    set -a prompt $(starship module git_branch | string trim)
+    set -a prompt $(starship module git_state | string trim)
+    set -a prompt $(starship module git_status | string trim)
+    set -a prompt $(starship module python | string trim)
+    set -a prompt $(starship module jobs | string trim)
+    set -a prompt "$(printf '\x1b[34m❯ ')"
+    echo -s "$prompt"
+  end
+
+  function starship_transient_rprompt_func
+    starship module time
+  end
+
+  starship init fish | source
+  enable_transience
 
   set fish_cursor_default block
   set fish_cursor_insert line
