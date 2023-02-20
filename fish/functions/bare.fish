021a2e7dad3a8edd255447e562899bfa6ed3fa79
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-19 22:27:45 -0300

    delete bare function

--- fish/functions/bare.fish
+++
@@ -1,3 +0,0 @@
-function bare --wraps='git --git-dir=/home/lucas/.dotfiles/ --work-tree=/home/lucas' --description 'manage dotfiles using a bare repository'
-  git --git-dir=/home/lucas/.dotfiles/ --work-tree=/home/lucas $argv; 
-end
