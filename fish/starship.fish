ee4ed6a9dd03736093998e3525c267b17da8cf5f
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-03-21 18:52:23 -0300

    add starship configs

---
+++ fish/starship.fish
@@ -0,0 +1,16 @@
+# Disable virtualenv prompt, it breaks starship
+set -g VIRTUAL_ENV_DISABLE_PROMPT 1
+
+# Remove default mode prompt
+builtin functions -e fish_mode_prompt
+
+set -gx STARSHIP_SHELL "fish"
+
+# Transience related functions
+function reset-transient --on-event fish_postexec
+    set -g TRANSIENT 0
+end
+
+# Set up the session key that will be used to store logs
+# We don't use `random [min] [max]` because it is unavailable in older versions of fish shell
+set -gx STARSHIP_SESSION_KEY (string sub -s1 -l16 (random)(random)(random)(random)(random)0000000000000000)
