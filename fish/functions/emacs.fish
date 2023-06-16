e8002e1eb3cb545ac7e078c840519343d95b4929
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-06-16 14:18:39 -0300

    delete emacs function

--- fish/functions/emacs.fish
+++
@@ -1,9 +0,0 @@
-function emacs --wraps=emacsclient --description 'open file in a running emacsclient'
-  if ps axo comm | grep ^emacsclient > /dev/null
-    emacsclient $argv &
-    disown $last_pid
-  else
-    emacsclient --create-frame --alternate-editor="" $argv &
-    disown $last_pid
-  end
-end
