db2cf71bea5ad0073791f419bccee1ca64ed4965
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-07-27 15:53:09 -0300

    add collation

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,5 +1,6 @@
 function ls --wraps=ls --description 'list directory contents'
   dircolors -c | sed 's/setenv/set -fx/1' | source
+  set -fx LC_COLLATE C
 
   command ls --group-directories-first --color=auto $argv
 end
