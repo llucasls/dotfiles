a93000651c0e51d851dcea9cb189139d13b7d367
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-09-20 12:05:26 -0300

    add relative numbers

--- emacs/init.el
+++ emacs/init.el
@@ -12,7 +12,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tango-dark)))
+ '(custom-enabled-themes '(tango-dark))
+ '(display-line-numbers 'relative))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
