e1f940df65e65629734e1ad596395683e0b9e594
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-09-21 08:33:58 -0300

    update init file

--- emacs/init.el
+++ emacs/init.el
@@ -6,24 +6,12 @@
 
 (load-module "conf.d/melpa-packages")
 
-(setq inhibit-startup-screen nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tango-dark))
- '(display-line-numbers 'relative)
- '(scroll-down-aggressively 0.0)
- '(scroll-up-aggressively 0.0))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (locate-user-emacs-file "conf.d/custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
 
-(setq scroll-margin 12)
+(setq inhibit-startup-screen nil)
+
+(setq scroll-margin 14)
 
 (setq-default show-trailing-whitespace t) ;; Show trailing whitespace
 (setq-default whitespace-style '(face tabs tab-mark trailing lines-tail)) ;; Display tab characters
