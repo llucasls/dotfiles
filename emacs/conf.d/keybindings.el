3b6d2ed5c469994492d56ee2e537c31b8c838609
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-10-07 18:18:49 -0300

    correct warnings

--- emacs/conf.d/keybindings.el
+++ emacs/conf.d/keybindings.el
@@ -38,15 +38,15 @@
 (evil-set-initial-state 'Info-mode 'emacs)
 
 (defun insert-tab-action () (interactive)
-  (if (looking-back "^\s*")
+  (if (looking-back "^\s*" nil)
     (evil-shift-right-line 1)
     (insert "\t")))
 
 (define-key evil-insert-state-map (kbd "TAB") 'insert-tab-action)
 
 (defun insert-del-action () (interactive)
-  (if (looking-back "^\s*")
+  (if (looking-back "^\s*" nil)
     (evil-shift-left-line 1)
-    (delete-backward-char 1)))
+    (delete-char -1)))
 
 (define-key evil-insert-state-map (kbd "DEL") 'insert-del-action)
