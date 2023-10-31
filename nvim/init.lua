adca24ad69319b18fe5f2dcfff31812064ae68aa
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-10-31 00:04:58 -0300

    replace keybindings.vim with keybindings.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,10 +3,10 @@ function Source(file)
   vim.cmd(string.format('source %s/%s', nvimdir, file))
 end
 
-Source 'plugins.vim'
-Source 'options.vim'
-Source 'keybindings.vim'
-Source 'run_buffer.vim'
-Source 'search.vim'
-Source 'caps_lock.vim'
-Source 'help.vim'
+Source  'plugins.vim'
+Source  'options.vim'
+require 'keybindings'
+Source  'run_buffer.vim'
+Source  'search.vim'
+Source  'caps_lock.vim'
+Source  'help.vim'
