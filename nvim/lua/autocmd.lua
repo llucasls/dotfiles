2a95fc163efc9de09910ea7ac9c16fba6dd37a0e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-11-01 07:57:49 -0300

    create group for auto commands

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -3,7 +3,10 @@ local function set_scrolloff()
   vim.o.scrolloff = string.format('%.f', new_height / 3) + 0
 end
 
+local automate = vim.api.nvim_create_augroup('automate', { clear = true })
+
 vim.api.nvim_create_autocmd('VimResized', {
   callback = set_scrolloff,
-  desc = 'Reset the scrolloff option on every window resize'
+  desc = 'Reset the scrolloff option on every window resize',
+  group = automate,
 })
