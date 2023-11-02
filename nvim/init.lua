e5a4841e72857240ecf2848232fcd32ca9240e8e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-11-02 02:45:37 -0300

    remove helper function to source Vimscript

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,3 @@
-local nvimdir = vim.fn.stdpath('config')
-local function source(file)
-  vim.cmd(string.format('source %s/%s', nvimdir, file))
-end
-
 require 'plugins'
 require 'options'
 require 'keybindings'
