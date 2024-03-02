07cb4ca9475474f7710ed677e18b6580305dd06f
Lucas L. S. Haine <lucaslshaine@gmail.com>
2024-03-02 19:00:40 -0300

    add keybinding to open emacs tutorial

--- emacs/conf.d/keybindings.el
+++ emacs/conf.d/keybindings.el
@@ -67,3 +67,41 @@
 (keymap-set evil-insert-state-map "DEL" 'insert-del-action)
 
 (keymap-set evil-normal-state-map (leader "t") 'my-open-terminal)
+
+(defvar tutorial-file nil
+  "This variable holds the name of the tutorial file. It is expected to change
+  every time the tutorial is opened, since it is a temporary copy.")
+
+(defvar previous-buffer-query-functions nil
+  "This variable temporarily holds the value of kill-buffer-query-functions.
+  Its purpose is to safely restore kill-buffer-query-functions.")
+
+(defun open-temporary-copy ()
+  (interactive)
+  (let* ((original-file (expand-file-name "tutorial" user-emacs-directory))
+         (temp-file (make-temp-file "tutorial-" nil ".txt")))
+    (copy-file original-file temp-file t)
+    (find-file temp-file)
+    (setq tutorial-file temp-file)
+    (evil-emacs-state)
+    (setq previous-buffer-query-functions kill-buffer-query-functions)
+    (setq kill-buffer-query-functions nil)
+    (add-hook 'kill-buffer-hook #'delete-temporary-copy)))
+
+(defun exit-temporary-copy-emacs-mode ()
+  "Switch back to Evil mode when exiting the tutorial buffer."
+  (when (string= (buffer-name) "tutorial")
+    (evil-emacs-state -1)))
+
+(defun delete-temporary-copy ()
+  "Delete temporary file."
+  (when (and (boundp 'tutorial-file) tutorial-file)
+    (delete-file tutorial-file)
+    (setq tutorial-file nil))
+  (remove-hook 'kill-buffer-hook #'delete-temporary-copy)
+  (setq kill-buffer-query-functions previous-buffer-query-functions)
+  (setq previous-buffer-query-functions nil))
+
+(add-hook 'kill-buffer-hook #'exit-temporary-copy-emacs-mode)
+
+(keymap-set evil-normal-state-map "C-h t" 'open-temporary-copy)
